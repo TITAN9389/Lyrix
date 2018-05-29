cdf9c5d5e5b52c5db7eae0cb804d058d87b92faf
Seb Maz <seb.maz93@icloud.com>
2018-05-29 19:25:20 +0400

    apollo refetch queries

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
 	constructor(props) {
@@ -6,11 +10,26 @@ class SongCreate extends Component {
 
 		this.state = { title: '' };
 	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		this.props
+			.mutate({
+				variables: {
+					title: this.state.title
+				},
+				refetchQueries: [{ query }]
+			})
+			.then(() => hashHistory.push('/'));
+	}
+
 	render() {
 		return (
 			<div>
+				<Link to="/">Back</Link>
 				<h3>Create a New Song</h3>
-				<form>
+				<form onSubmit={this.onSubmit.bind(this)}>
 					<label>Song Title:</label>
 					<input
 						onChange={e => this.setState({ title: e.target.value })}
@@ -22,4 +41,12 @@ class SongCreate extends Component {
 	}
 }
 
-export default SongCreate;
+const mutation = gql`
+	mutation AddSong($title: String) {
+		addSong(title: $title) {
+			title
+		}
+	}
+`;
+
+export default graphql(mutation)(SongCreate);
