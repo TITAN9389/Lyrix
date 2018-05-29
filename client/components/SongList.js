7a3f908d60c5f77b1421910ea542513bb3df06b9
Seb Maz <seb.maz93@icloud.com>
2018-05-29 04:46:13 +0400

    Apollo Provider , add song

---
+++ client/components/SongList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+	renderSongs() {
+		return this.props.data.songs.map(song => (
+			<li key={song.id}>{song.title}</li>
+		));
+	}
+	render() {
+		if (this.props.data.loading) {
+			return <div>Loading...</div>;
+		}
+
+		return (
+			<div>
+				<h3>SongList</h3>
+				{this.renderSongs()}
+			</div>
+		);
+	}
+}
+
+const query = gql`
+	{
+		songs {
+			id
+			title
+		}
+	}
+`;
+
+export default graphql(query)(SongList);
