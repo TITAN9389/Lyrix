0b1be229b52cbebf2961169d5c2520b148775ea2
Seb Maz <seb.maz93@icloud.com>
2018-06-21 16:50:32 +0400

    fetch single song

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -5,9 +5,26 @@ import { Link } from 'react-router';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+	onSongDelete(id) {
+		this.props.mutate({
+			variables: {
+				id: id
+			},
+			refetchQueries: [{ query }]
+		});
+		// .then(() => this.props.data.refetch()); we can use this (2nd method)
+	}
 	renderSongs() {
-		return this.props.data.songs.map(song => (
-			<li key={song.id}>{song.title}</li>
+		return this.props.data.songs.map(({ id, title }) => (
+			<li key={id} className="collection-item">
+				{title}
+				<i
+					className="material-icons"
+					onClick={() => this.onSongDelete(id)}
+				>
+					delete
+				</i>
+			</li>
 		));
 	}
 	render() {
@@ -30,4 +47,12 @@ class SongList extends Component {
 	}
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+	mutation DeleteSong($id: ID) {
+		deleteSong(id: $id) {
+			id
+		}
+	}
+`;
+
+export default graphql(mutation)(graphql(query)(SongList));
